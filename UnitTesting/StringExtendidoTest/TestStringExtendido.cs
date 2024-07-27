abb0bbba030d09595f982f88c7bcacd4a6cacf66
Juan Beltrame <juan_beltrame@hotmail.com>
2024-07-27 06:50:47 -0300

    First Commit

---
+++ UnitTesting/StringExtendidoTest/TestStringExtendido.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringExtendidoTest
+{
+    [TestClass]
+    internal class TestStringExtendido
+    {
+        [TestMethod]
+        public void ContarPalabras_CuandoRecibeDosPalabras_DeberiaRetornarNumeroDos()
+        {
+           
+        }
+    }
+}
